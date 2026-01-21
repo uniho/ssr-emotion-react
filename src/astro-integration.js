e2c5fd6c729d4deabca5c8c1df595d3a1d7c7033
uniho <uniho@users.noreply.github.com>
2026-01-21 10:12:35 +0900

    core: fix for the ReferenceError: $RefreshSig$ is not defined

--- src/astro-integration.js
+++ src/astro-integration.js
@@ -6,7 +6,7 @@ export default function() {
   return {
     name: 'ssr-emotion-react',
     hooks: {
-      'astro:config:setup': ({ addRenderer, updateConfig, config }) => {
+      'astro:config:setup': ({ addRenderer, updateConfig, injectScript, config, command }) => {
         addRenderer({
           name: 'ssr-emotion-react',
           serverEntrypoint: 'ssr-emotion-react/astro/render',
@@ -17,9 +17,16 @@ export default function() {
           (p) => p && (p.name === 'vite:react-babel' || p.name === 'vite:react-jsx')
         );
 
+        if (command === 'dev') {
+          injectScript('before-hydration', `
+            window.$RefreshReg$ = window.$RefreshReg$ || (() => {});
+            window.$RefreshSig$ = window.$RefreshSig$ || (() => (type) => type);
+          `);
+        }
+
         updateConfig({
           vite: {
-            plugins: hasReact ? [] : [react()],
+            plugins: hasReact ? [] : [react({ fastRefresh: false })],
             ssr: {
               external: ['@emotion/css', '@emotion/server']
             }
